b127c808863fe6c17bbe00d54e115be05551efad
qfox <alex@qfox.ru>
2012-09-17 02:24:54 +0400

    web access point added

--- examples/index.php
+++ examples/index.php
@@ -8,30 +8,19 @@ require_once "../phpbem.php";
 $wwwpath = __DIR__ . 'www';
 
 // Create BEM renderer
-$renderer = new bemhtml($wwwpath, $cache_context = true, $use_exts = false);
+$bem = new bemhtml($wwwpath);
 
+// making dynamic context
+$context = array(
+	'url'   => 'http://example.com/',
+	'text'  => 'Привет',
+	'title' => function ($title) {
+		return sprintf('(%s) %s', $_SERVER['REQUEST_URI'], $title);
+	}
+);
+$env = $_REQUEST;
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        // Create some dynamic context
-        context = {'url' : 'http://example.com/',
-                   'text': 'Привет',
-                   'title': lambda title: '(%s) %s' % (self.request.uri, title)}# (path. request.uri)}
-        env = self.request
-        # Render page example from pages
-        # Calls BEMHTML.apply(render(context, env))
-        message = renderer.render('pages/example', context, env, "render",
-                                  extra_files=['example.en.js'])
-        self.write(message)
+// render it
+$bem->page('pages/example', array('example.en.js'));
+echo $bem->render($context, $env, 'render');
 
-/**
-if __name__ == "__main__":
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(wwwpath, 'pages')}),
-        (r"/blocks/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(wwwpath, 'blocks')}),
-        (r"/bem-bl/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(wwwpath, 'bem-bl')}),
-    ])
-    application.listen(3000)
-    tornado.ioloop.IOLoop.instance().start()
-*/
