6c2020dc670c0d0bf59a8a4c6e878282a23039ba
qfox <alex@qfox.ru>
2012-09-22 04:10:27 +0400

    Libraries

---
+++ custom-levels-example/app/bemyeah/pages/index/index.bemjson.js
@@ -0,0 +1,40 @@
+({
+    block: 'b-page',
+    title: 'Title of the page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_index.css', ie: false },
+        { elem: 'css', url: '_index', ie: true },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: '_index.js' },
+        { elem: 'meta', attrs: { name: 'description', value: '' }},
+        { elem: 'meta', attrs: { name: 'keywords', value: '' }}
+    ],
+    content:[
+        {
+            block: 'header',
+            content: [
+                'header content goes here'
+            ]
+        },
+        {
+            block: 'content',
+            content: [
+                {
+                    block: 'b-some',
+                    mix: ['i-verbatile'],
+                    content: [
+                        'some verbatile content'
+                    ]
+                },
+                'main content'
+            ]
+        },
+        {
+            block: 'footer',
+            content: [
+                'footer content goes here'
+            ]
+        }
+    ]
+})
